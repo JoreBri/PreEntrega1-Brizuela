e5dc2a9565f6102877e96bb7e17d2f2de9aab578
JoreBri <jorebrizuela.jb@gmail.com>
2024-03-12 20:17:28 -0300

    uno mas

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,6 @@
 let nombreIngresado = prompt("Ingrese su nombre");
 
 //Al ingresar el nombre se saluda al cliente y se le informa lo que debe hacer
-alert("Bienvenido " + nombreIngresado + ' a Unicornio Nails);
+alert("Bienvenido " + nombreIngresado +  "a Unicornio Nails");
 
 alert("¡A continuación podrás elegir el servicio que quieras realizarte!");
