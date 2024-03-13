c453f5d578b12bfe6e3825926e6a42adcd8833d3
JoreBri <jorebrizuela.jb@gmail.com>
2024-03-13 17:08:28 -0300

    mas commit

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,3 @@
-// Al entrar la página, se muestra un prompt que pide el nombre del cliente
-let nombreIngresado = prompt("Ingrese su nombre");
 //Declaro las variables
 let servicio; 
 let precioManicuraRusa = 5000;
@@ -7,13 +5,14 @@ let precioEsmaltadoSemiLiso = 8500;
 let precioEsmaltadoSemiDiseño = 10000;
 let precioCappingLiso = 9500;
 let precioCappingdiseño = 10500;
-let precioEsculpidas = 10000;
-let precioEsclpidastalleLlisas = 10500;
-let precioEsculpidasTalleMLisas = 11000;
-let precioEsclpidastalleLDiseño = 12000;
-let precioEsclpidastalleMDiseño = 12500;
+let precioEsculpidasLisas = 11000;
+let precioEsclpidasDiseño = 12500;
 let total = 0;
 let salirDeLaWeb = true;
+//POR FAVOR SEBA AYUDAME ;(
+
+// Al entrar la página, se muestra un prompt que pide el nombre del cliente
+let nombreIngresado = prompt("Ingrese su nombre");
 
 while (nombreIngresado == null || !(nombreIngresado.length >= 3)) {
     nombreIngresado = prompt("Por favor, ingrese un nombre,para brindarle la mejor atención!");
@@ -27,52 +26,51 @@ alert("Bienvenido " + nombreIngresado +  " a ¡Unicornio Nails!");
 alert("¡A continuación podrás elegir el servicio que quieras realizarte!");
 
 //Comenzamos a elegir los servicios
-
-
+//Primero ofrecemos las 4 opciones
 do {
-    servicio = prompt("Ingrese el número de la opción que desea comprar: \n1.Manicura Rusa\n2. Esmaltado semipermanente \n3. Capping \n4.Esculpidas \n0. Salir");
-
+    servicio = prompt("Ingrese el número de la opción que desea realizarce: \n1.Manicura Rusa\n2. Esmaltado semipermanente \n3. Capping \n4.Esculpidas \n0. Salir");
+//Nos aseguramos de que elija una opcion valida 
     if (isNaN(servicio)) {
       alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 4 para un servicio a realizarse y 0 para salir");
       continue;
     } else {
       servicio = parseInt(servicio);
       if ((servicio < 0) || (servicio > 4)) {
-        alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 4 para un servicio a realizarsey 0 para salir");
+        alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 4 para un servicio a realizarse y 0 para salir");
         continue;
       }
     }
-
-    if (servicio !== 1) {
-      do {
-        cantidad = parseInt(prompt("Ingrese la cantidad de prendas que desea comprar:"));
-        if (isNaN(cantidad) || cantidad < 0) {
-          alert("Por favor, ingrese un número válido y mayor o igual a cero.");
-        }
-      } while (isNaN(cantidad) || cantidad < 0)
-    }
-
-    switch (opcion) {
+//Si elije 1 le pasamos el precio
+    switch (servicio) {
       case 1:
-        totalGastado += remera * cantidad;
-        alert("Ha comprado " + cantidad + " remeras por $" + remera * cantidad);
+        alert("Usted quiere realizarse Manicura Rusa por $" + precioManicuraRusa);
         break;
+//Si elije 2, 3 0 4 le damos opciones
       case 2:
-        totalGastado += buzo * cantidad;
-        alert("Ha comprado " + cantidad + " buzos por $" + buzo * cantidad);
-        break;
+        servicio = parseInt(prompt("Elija si quiere \n1. esmaltado liso \n2.esmaltado con deseño"));
+        if (servicio === 1) {
+            alert( "Usted quiere realizarse Esmaltado Semipermanente liso por $" + precioEsmaltadoSemiLiso)
+          }
+          else (servicio === 2);{{
+            alert( "Usted quiere realizarse Esmaltado Semipermanente con diseño por $" + precioEsmaltadoSemiDiseño)
+          }}
       case 3:
-        totalGastado += campera * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + campera * cantidad);
+        servicio = parseInt(prompt("Elija si quiere \n1. Capping liso \n2.Capping con deseño"));
+        if (servicio === 1) {
+            alert( "Usted quiere realizarse Capping liso por $" + precioCappingLiso)
+          }
+          else (servicio === 2);{{
+            alert( "Usted quiere realizarse Capping con diseño por $" + precioCappingdiseño)
+          }}
         break;
       case 4:
-        totalGastado += pantalon * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + pantalon * cantidad);
-        break
-      case 5:
-        totalGastado += zapatillas * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + zapatillas * cantidad);
-        break
+        servicio = parseInt(prompt("Elija si quiere \n1. Esculpidas lisas \n2.Esculpidas con deseño"));
+        if (servicio === 1) {
+            alert( "Usted quiere realizarse Esculpidas lisas por $" + precioEsculpidasLisas)
+          }
+          else (servicio === 2);{{
+            alert( "Usted quiere realizarse Esculpidas con diseño por $" + precioEsclpidasDiseño)
+          }}
       case 0:
         alert("Saliendo...");
         break;
@@ -81,30 +79,4 @@ do {
     }
   } while (opcion !== 0);
 
-  alert("El total gastado es de $" + totalGastado + ".");
   alert("Gracias por elegirnos. Vuelva pronto.");
-
-
-carritoDeCompras();
-
-// while () {
-//     const servicioElegido = prompt("Ingrese el número del servicios que desea realizarse:\n1.Manicura Rusa\n2. Esmaltado semipermanente \n3. Capping \n4.Esculpidas \nSi desea salir, por favor ingrese 0:");
-
-//     switch (servicioElegido) {
-//         case "1":
-            
-//         case "2":
-//             total += precioEsmaltadoSemiLiso;
-//             alert("Excelente, usted ha agregado una sudadera.");
-//             break;
-//         case "3":
-//             total += precioGorras;
-//             alert("Excelente, usted ha agregado una gorra.");
-//             break;
-//         case "0":
-//             salirDeLaWeb = false;
-//             break;
-//         default:
-//             alert("Opción inválida. Por favor, elija un número válido o escriba '0' para finalizar la compra.");
-//     }
-// }
